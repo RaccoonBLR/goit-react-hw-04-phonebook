10f22778c76681335daacff92e461304da7e708a
Oleksandr <107481048+RaccoonBLR@users.noreply.github.com>
2023-03-16 01:24:49 +0200

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,79 +1,64 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './Container/Container';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { ContactItem } from './ContactItem/ContactItem';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(getLocalContacts() ?? []);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      this.setState({ contacts });
-    }
+  useEffect(() => {
+    const savedContacts = getLocalContacts();
+    savedContacts && setContacts(savedContacts);
+  }, []);
+
+  useEffect(() => {
+    setLocalContacts(contacts);
+  }, [contacts]);
+
+  function getLocalContacts() {
+    return JSON.parse(localStorage.getItem('contacts'));
+  }
+  function setLocalContacts(contacts) {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
   }
 
-  componentDidUpdate(prevProps, { contacts: prevContacts }) {
-    const { contacts } = this.state;
-
-    if (contacts !== prevContacts) {
-      localStorage.setItem('contacts', JSON.stringify(contacts));
-    }
-  }
-
-  handleAppend = newContact => {
+  const handleAppend = newContact => {
     const { name: newName } = newContact;
-    const { contacts } = this.state;
 
-    return contacts.some(({ name }) => name === newName)
+    contacts.some(({ name }) => name === newName)
       ? alert(`${newName} is already in contacts.`)
-      : this.setState(({ contacts }) => ({
-          contacts: [...contacts, newContact],
-        }));
+      : setContacts(state => [...state, newContact]);
   };
 
-  handleChange = evt => {
-    const { name, value } = evt.currentTarget;
-
-    this.setState({ [name]: value });
+  const handleChange = evt => {
+    const { value } = evt.currentTarget;
+    setFilter(value);
   };
 
-  handleDelete = evt => {
-    const { id: deleteId } = evt.target;
+  const handleDelete = evt => {
+    const removableId = evt.target.id;
 
-    this.setState(({ contacts }) => {
-      const updateContacts = contacts.filter(({ id }) => !(id === deleteId));
-
-      return {
-        contacts: [...updateContacts],
-      };
-    });
+    setContacts(state => state.filter(({ id }) => id !== removableId));
   };
 
-  render() {
-    const { contacts, filter } = this.state;
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm handleAppend={handleAppend} />
 
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm handleAppend={this.handleAppend} />
+      <h2>Contacts</h2>
+      <Filter handleChange={handleChange} filter={filter} />
 
-        <h2>Contacts</h2>
-        <Filter handleChange={this.handleChange} filter={filter} />
-
-        <ContactList>
-          <ContactItem
-            contacts={contacts}
-            filter={filter}
-            handleDelete={this.handleDelete}
-          />
-        </ContactList>
-      </Container>
-    );
-  }
-}
+      <ContactList>
+        <ContactItem
+          contacts={contacts}
+          filter={filter}
+          handleDelete={handleDelete}
+        />
+      </ContactList>
+    </Container>
+  );
+};
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -4,5 +4,5 @@ import { PropTypes } from 'prop-types';
 export const Container = ({ children }) => <Wrapper>{children}</Wrapper>;
 
 Container.propTypes = {
-  children: PropTypes.arrayOf(PropTypes.element.isRequired).isRequired,
+  children: PropTypes.node.isRequired,
 };
